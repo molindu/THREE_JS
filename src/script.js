4f87dec824863c589d0d77fe03cc737f06d62edb
molindu <molinduyasodha@gmail.com>
2026-06-21 19:34:32 +0530

    Rotation- part1

--- src/script.js
+++ src/script.js
@@ -5,48 +5,52 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 const scene = new THREE.Scene()
 
 // add objects to the scene
-const cubeGeometry = new THREE.BoxGeometry(1, 1,)
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red" })
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red", wireframe: true })
 
 const cubeMesh = new THREE.Mesh(
   cubeGeometry,
   cubeMaterial
-)
+)          
+cubeMesh.position.y =1;   
+cubeMesh.position.x =6;   
 
 
-const cubeMesh2 = new THREE.Mesh(
-  cubeGeometry,
-  cubeMaterial
-)
-const cubeMesh3 = new THREE.Mesh(
-  cubeGeometry,
-  cubeMaterial
-)
+// const cubeMesh2 = new THREE.Mesh(
+//   cubeGeometry,
+//   cubeMaterial
+// )
+// const cubeMesh3 = new THREE.Mesh(
+//   cubeGeometry,
+//   cubeMaterial
+// )
 
 const group = new THREE.Group();
-group.add(cubeMesh);
-group.add(cubeMesh2);
-group.add(cubeMesh3);
+// group.add(cubeMesh);
+// group.add(cubeMesh2);
+// group.add(cubeMesh3);
 
 group.position.y = 2;
 group.scale.setScalar(2)
-scene.add(group)
-// scene.add(cubeMesh);
+// scene.add(group)
+scene.add(cubeMesh);
 
 // cubeMesh.scale.y = 2;
 // cubeMesh.scale.set(2, 2, 1)
 
-cubeMesh.position.x = 2
-cubeMesh2.position.x = 0 
-cubeMesh2.position.y = -1
-cubeMesh2.scale.setScalar(0.5);
-cubeMesh3.position.x = -2
+// cubeMesh.position.x = 2
+// cubeMesh2.position.x = 0 
+// cubeMesh2.position.y = -1
+// cubeMesh2.scale.setScalar(0.5);
+// cubeMesh3.position.x = -2
 
 // const tempVector = new THREE.Vector3(1, 1, 1)
 // cubeMesh.position.copy(tempVector)
 
 const axesHelper = new THREE.AxesHelper(2)
-scene.add(axesHelper)
+cubeMesh.add(axesHelper)
+// scene.add(axesHelper)
+
 
 // initialize the camera
 const camera = new THREE.PerspectiveCamera(
