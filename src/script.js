13c81f5ff63090610fb0990fea04e910157efec2
molindu <molinduyasodha@gmail.com>
2026-06-21 11:09:45 +0530

    scene hierachy

--- src/script.js
+++ src/script.js
@@ -12,24 +12,45 @@ const cubeMesh = new THREE.Mesh(
   cubeGeometry,
   cubeMaterial
 )
-scene.add(cubeMesh);
+
+
+const cubeMesh2 = new THREE.Mesh(
+  cubeGeometry,
+  cubeMaterial
+)
+const cubeMesh3 = new THREE.Mesh(
+  cubeGeometry,
+  cubeMaterial
+)
+
+const group = new THREE.Group();
+group.add(cubeMesh);
+group.add(cubeMesh2);
+group.add(cubeMesh3);
+
+group.position.y = 2;
+group.scale.setScalar(2)
+scene.add(group)
+// scene.add(cubeMesh);
 
 // cubeMesh.scale.y = 2;
-cubeMesh.scale.set(2, 2, 1)
+// cubeMesh.scale.set(2, 2, 1)
 
-// cubeMesh.position.x = 1
-// cubeMesh.position.y = 1 
-// cubeMesh.position.z = 1
+cubeMesh.position.x = 2
+cubeMesh2.position.x = 0 
+cubeMesh2.position.y = -1
+cubeMesh2.scale.setScalar(0.5);
+cubeMesh3.position.x = -2
 
-const tempVector = new THREE.Vector3(1, 1, 1)
-cubeMesh.position.copy(tempVector)
+// const tempVector = new THREE.Vector3(1, 1, 1)
+// cubeMesh.position.copy(tempVector)
 
 const axesHelper = new THREE.AxesHelper(2)
 scene.add(axesHelper)
 
 // initialize the camera
 const camera = new THREE.PerspectiveCamera(
-  70, 
+  70,
   window.innerWidth / window.innerHeight,// aspect ratio
   0.1,// near
   30) // property
@@ -47,7 +68,7 @@ const renderer = new THREE.WebGLRenderer({
   antialias: true
 })
 renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.setPixelRatio( Math.min(window.devicePixelRatio, 2))
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 // console.log(camera)
 //initialize the controls
@@ -56,8 +77,8 @@ const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 controls.autoRotate = true
 
-window.addEventListener('resize',()=>{
-    camera.aspect = window.innerWidth / window.innerHeight;
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight)
 })
